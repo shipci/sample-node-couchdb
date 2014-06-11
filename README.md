4bcbcb7d0be85171023e02320511e72223e6d8e4
Patrick <oneredoak@gmail.com>
2014-06-10 19:22:30 -0700

    Copied sample-node-mongo

---
+++ README.md
@@ -0,0 +1,6 @@
+Node.js MongoDB Sample [![Build Status](https://apibeta.shippable.com/projects/5373eb2df39baf5a00c05c89/badge/master)](https://beta.shippable.com/projects/5373eb2df39baf5a00c05c89)
+=================
+
+Implements a simple web application using MongoDB, with tests.
+
+Uses Grunt to run tests against an Express server, then generates reports with Xunit and Istanbul.
---
+++ test.js
@@ -0,0 +1,37 @@
+var superagent = require("superagent"),
+    chai = require("chai"),
+    expect = chai.expect,
+    should = require("should");
+
+describe("Index", function () {
+  it("renders HTML", function (done) {
+    superagent.get("http://localhost:3000/")
+      .end(function (e, res) {
+        (e === null).should.equal(true);
+        res.text.should.equal("Hey buddy!");
+        done();
+      });
+  });
+});
+
+describe("Persistence", function () {
+  it("should create a thing", function (done) {
+    superagent.get("http://localhost:3000/doobie")
+      .end(function (e, res) {
+        (e === null).should.equal(true);
+        var response = (res.body.indexOf("new") !== -1);
+        expect(response).to.equal(false);
+        done();
+      });
+  });
+  it("should retrieve a thing", function (done) {
+    superagent.get("http://localhost:3000/doobie")
+      .end(function (e, res) {
+        (e === null).should.equal(true);
+        var response = res.body;
+        response = response[0];
+        response.should.have.property("name", "doobie");
+        done();
+      });
+  });
+});
